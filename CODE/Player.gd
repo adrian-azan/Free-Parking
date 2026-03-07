6d80f01565311017de369260f4729b7b3b9efd73
Groot <adrian.azan@hotmail.com>
2026-03-07 16:02:08 -0500

    Movement Script is pretty much set

--- CODE/Player.gd
+++ CODE/Player.gd
@@ -1,6 +1,4 @@
-extends Node2D
-
-class_name Player
+class_name Player extends Node2D
 
 var coordinates: Vector2
 var audio_player: AudioStreamPlayer2D
@@ -14,11 +12,10 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	DEBUG_coordinatesLabel.text = "(%d, %d)" % [coordinates.x, coordinates.y]
+	DEBUG_coordinatesLabel.text = "(%d, %d)" % [coordinates.x, coordinates.y]	
 	
 	
-	var moving: Vector2 = Vector2.ZERO
-	
+	var moving: Vector2 = Vector2.ZERO	
 	if Input.is_action_pressed("move_up"):
 		moving.y -= .1
 	if Input.is_action_pressed("move_down"):
@@ -31,6 +28,8 @@ func _process(delta: float) -> void:
 
 	# Per Frame Actions
 	###################		
+	
+	#Animate movement
 	if Input.is_action_just_pressed("move_left"):
 		create_tween().tween_property($Sprite2D, "skew", .36, .2)
 		create_tween().tween_property($Sprite2D, "skew", 0, .2).set_delay(.2)
@@ -43,12 +42,15 @@ func _process(delta: float) -> void:
 
 	#When Pressed Actions
 	#####################
+	
+	#Play sound when moving
 	if moving != Vector2.ZERO and audio_player.playing == false:
 		audio_player.stream = ResourceLoader.load("res://ART/SOUND/Vroom noise.mp3")
 		audio_player.play()
 	elif moving == Vector2.ZERO:
 		audio_player.stop()
 	
+	#Animate movement
 	if Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("move_down"):
 		create_tween().tween_property($Sprite2D, "scale", Vector2(.03, .025), .1)
 		create_tween().tween_property($Sprite2D, "scale", Vector2(.03,.03), .1).set_delay(.1)
